9f7b9044129bd395de29119782eed1122b8c2abd
Francisca Martinez <franciscamartinezbcn@gmail.com>
2021-01-14 10:19:56 +0100

    "Primer commit - nou projecte Java"

---
+++ Escritorio/ENTORNS/GIT/Git_Video/src/Francisca/imprimirNum.java
@@ -0,0 +1,15 @@
+package Francisca;
+
+public class imprimirNum {
+public static void main(String[] args) {
+	System.out.println ("Numeros del 1 al 20 :");
+	System.out.println ("");
+	mostrar20Num();
+
+}
+public static void mostrar20Num() {
+	for (int i = 1; i <= 20; i++) {
+		System.out.print(i +" ");
+	}
+}
+}
