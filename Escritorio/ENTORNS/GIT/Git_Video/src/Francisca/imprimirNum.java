23813fd71914ff2e4875a6edb664664feca43124
Francisca Martinez <franciscamartinezbcn@gmail.com>
2021-01-14 11:07:47 +0100

    "Segon commit amb canvi fet"

--- Escritorio/ENTORNS/GIT/Git_Video/src/Francisca/imprimirNum.java
+++ Escritorio/ENTORNS/GIT/Git_Video/src/Francisca/imprimirNum.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 }
 public static void mostrar20Num() {
 	for (int i = 1; i <= 20; i++) {
-		System.out.print(i +" ");
+		System.out.print(i +"Canvi fet ");
 	}
 }
 }
