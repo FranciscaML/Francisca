351c3a4a370fcded535337052ef295c1abf258ef
Francisca Martinez <franciscamartinezbcn@gmail.com>
2021-01-19 14:36:45 +0100

    "primer commit nou projecte Java"

---
+++ Escritorio/ENTORNS/ProjectesEclipse/tema4/src/Francisca/activitat7.java
@@ -0,0 +1,16 @@
+package Francisca;
+
+public class activitat7 {
+
+		 public static void main (String[] args){
+	         System.out.println ("Numeros del 1 al 20");
+	         mostrar1_al_20();
+	     }
+	     public static void mostrar1_al_20() {
+	         for (int i =1; i<=20; i++) {
+	             System.out.print(i +" ");
+	         }
+	     }
+
+	}
+
