0426b09820941c89f7ca66d1d9dbc8e5f6dfb0a8
Francisca Martinez <franciscamartinezbcn@gmail.com>
2021-01-19 14:42:36 +0100

    "segon commit delprojecte java"

--- Escritorio/ENTORNS/ProjectesEclipse/Tema4/src/Francisca/activitat7.java
+++ Escritorio/ENTORNS/ProjectesEclipse/Tema4/src/Francisca/activitat7.java
@@ -4,7 +4,7 @@ public class activitat7 {
 
 	public static void main(String[] args) {
 		
-		 System.out.println ("Numeros del 1 al 20");
+		 System.out.println ("Numeros del 1 al 20 enters");
          mostrar1_al_20();
      }
      public static void mostrar1_al_20() {
